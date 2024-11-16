8b21c01c8909f6fcef48d518b03fbd04392710a1
jekutinamo23 <157634644+jekutinamo23@users.noreply.github.com>
2024-11-15 21:15:32 -0500

    Initial commit

---
+++ src/main/java/SkillBuilder5.java
@@ -0,0 +1,21 @@
+import java.util.Random;
+
+public class SkillBuilder5 {
+    private static Random rand = new Random(17);
+
+    public static boolean playCraps(){
+        //TODO: replace this line with your code.
+    }
+
+    private static void generateLevel(int level, int[] a){
+        // TODO: replace this line with your code.
+    }
+
+    private static String generateLevelString(int level, int[] a){
+        // TODO: replace this line with your code.
+    }
+
+    public static String pascalsTriangle(int numOfRows){
+        // TODO: replace this line with your code.
+    }
+}
